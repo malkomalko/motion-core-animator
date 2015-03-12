8b797aa1e243d19c942cba1bc7feed24118c82b3
Robert Malko <robmalko@gmail.com>
2015-03-12 11:55:18 -0400

    add failing animation with callback

--- Rakefile
+++ Rakefile
@@ -11,4 +11,6 @@ end
 Motion::Project::App.setup do |app|
   # Use `rake config' to see complete project settings.
   app.name = 'motion-core-animator'
+  app.resources_dirs << Dir['coreanimator/images']
+  app.vendor_project("coreanimator", :static)
 end
--- app/app_delegate.rb
+++ app/app_delegate.rb
@@ -1,15 +1,30 @@
 class AppDelegate
-  def application(application, didFinishLaunchingWithOptions:launchOptions)
-    rootViewController = UIViewController.alloc.init
-    rootViewController.title = 'motion-core-animator'
-    rootViewController.view.backgroundColor = UIColor.whiteColor
 
-    navigationController = UINavigationController.alloc.initWithRootViewController(rootViewController)
+  def application(application, didFinishLaunchingWithOptions:launchOptions)
+    @rootViewController = UIViewController.alloc.init
+    @rootViewController.title = 'motion-core-animator'
+    @rootViewController.view.backgroundColor = UIColor.whiteColor
+
+    navigationController = UINavigationController.alloc.initWithRootViewController(@rootViewController)
 
     @window = UIWindow.alloc.initWithFrame(UIScreen.mainScreen.bounds)
     @window.rootViewController = navigationController
     @window.makeKeyAndVisible
 
+    add_animation_view
+
     true
   end
+
+  def add_animation_view
+    w, h = UIScreen.mainScreen.bounds.size.to_a
+    @av = ExplosionView.new
+    @av.center = [w/2, h/2]
+    @rootViewController.view.addSubview(@av)
+
+    @av.addExplodeAnimationWithCompletion(->(finished) {
+      puts "finished the animation"
+    })
+  end
+
 end
